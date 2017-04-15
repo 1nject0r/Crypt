228b8602a5f1b68639167972a650ea4b805f2c66
1nject0r <mails4internet@gmail.com>
2017-04-15 21:58:06 +0200

    vigenere added brute

--- Vigenere.py
+++ Vigenere.py
@@ -25,11 +25,16 @@ def brute(password_list, msg):
 def main():
     pwd = str(raw_input('Enter your password:\n'))
     msg = str(raw_input('Enter your message:\n'))
+    pw_list = map(str, raw_input('Enter passwords:\n').split())
     enc_msg = encrypt(pwd, msg)
     dec_msg = decrypt(pwd, enc_msg)
+    bruted_msg = brute(pw_list, enc_msg)
 
     print 'Your encrypted message is:\n' + enc_msg + '\n\n'
     print 'Your decrypted message is:\n' + dec_msg + '\n\n'
+    print 'Your bruteforced messages are:'
+    for msg in bruted_msg:
+        print msg
 
 
 if __name__ == '__main__':
